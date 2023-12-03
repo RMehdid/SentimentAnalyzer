ba069affa117c4e42e4e2d73167b79c7896c6c85
Samy Abderraouf Mehdid <sr.mehdid@gmail.com>
2023-12-03 11:54:56 +0100

    Initial Commit

---
+++ SentimentAnalysisApp/SentimentAnalysisAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentAnalysisAppApp.swift
+//  SentimentAnalysisApp
+//
+//  Created by Samy Mehdid on 3/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SentimentAnalysisAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
